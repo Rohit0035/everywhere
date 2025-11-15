447a9ee55d371199949b811bba0a9c7b0b081438
Patel (EXT), Dayalu dn73wt <dayalupatel1998@gmail.com>
2025-11-15 22:24:10 +0530

    Production ready code

--- src/components/BookStorage.js
+++ src/components/BookStorage.js
@@ -5,12 +5,12 @@ const BookStorageForm = () => {
     return (
         <section className="book-storage-form py-5" id="book">
             <div className="container custom-container">
-                <div class="row">
-                    <div class="col-md-5">
+                <div className="row">
+                    <div className="col-md-5 d-none d-md-block">
                         <img src="assets/img/common/bookimg.png" alt="" />
                     </div>
-                    <div class="col-md-7">
-                        <div class="bg-main p-4 rounded" style={{ backgroundColor: '#003158' }}>
+                    <div className="col-md-7">
+                        <div className="bg-main p-4 rounded" style={{ backgroundColor: '#003158' }}>
                             <h2 className="title text-white">
                                 Book Your <span> Storage</span>
                             </h2>
@@ -22,47 +22,47 @@ const BookStorageForm = () => {
                                 >
                                     <button
                                         className='nav-link active text-white'
-                                        id='nav-mission-tab'
-                                        data-bs-toggle='tab'
-                                        data-bs-target='#nav-mission'
-                                        type='button'
-                                        role='tab'
-                                        aria-controls='nav-mission'
-                                        aria-selected='true'
-                                    >
-                                        Pick Up
-                                    </button>
-                                    <button
-                                        className='nav-link text-white'
                                         id='nav-vision-tab'
                                         data-bs-toggle='tab'
                                         data-bs-target='#nav-vision'
                                         type='button'
                                         role='tab'
                                         aria-controls='nav-vision'
-                                        aria-selected='false'
+                                        aria-selected='true'
                                     >
                                         Drop
+                                    </button>
+                                    <button
+                                        className='nav-link text-white'
+                                        id='nav-mission-tab'
+                                        data-bs-toggle='tab'
+                                        data-bs-target='#nav-mission'
+                                        type='button'
+                                        role='tab'
+                                        aria-controls='nav-mission'
+                                        aria-selected='false'
+                                    >
+                                        Pick Up
                                     </button>
                                 </div>
                             </nav>
                             <div className='tab-content' id='nav-tabContent'>
                                 <div
                                     className='tab-pane fade show active'
+                                    id='nav-vision'
+                                    role='tabpanel'
+                                    aria-labelledby='nav-vision-tab'
+                                >
+                                    <DropCustomerForm />
+                                </div>
+                                <div
+                                    className='tab-pane fade'
                                     id='nav-mission'
                                     role='tabpanel'
                                     aria-labelledby='nav-mission-tab'
                                 >
 
                                     <PickupCustomerForm/>
-                                </div>
-                                <div
-                                    className='tab-pane fade'
-                                    id='nav-vision'
-                                    role='tabpanel'
-                                    aria-labelledby='nav-vision-tab'
-                                >
-                                    <DropCustomerForm />
                                 </div>
                             </div>
                         </div>
